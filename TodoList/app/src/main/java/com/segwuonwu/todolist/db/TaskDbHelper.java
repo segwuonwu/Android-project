5f4b6af6d5d4990613735d056317139e6d1ae14b
segwuonwu <segwuonwu@gmail.com>
2020-01-31 13:25:22 -0800

    Todolist andriod project

---
+++ TodoList/app/src/main/java/com/segwuonwu/todolist/db/TaskDbHelper.java
@@ -0,0 +1,34 @@
+package com.segwuonwu.todolist.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class TaskDbHelper extends SQLiteOpenHelper {
+
+    public TaskDbHelper(Context context) {
+        super(context, TaskContract.DB_NAME, null, TaskContract.DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //SQL query
+        /*CREATE TABLE tasks (
+                _id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title TEXT NOT NULL
+        );*/
+
+        //Java implementation of the query
+        String createTable = "CREATE TABLE " + TaskContract.TaskEntry.TABLE + " ( " +
+                TaskContract.TaskEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                TaskContract.TaskEntry.COL_TASK_TITLE + " TEXT NOT NULL);";
+
+        db.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TaskContract.TaskEntry.TABLE);
+        onCreate(db);
+    }
+}
